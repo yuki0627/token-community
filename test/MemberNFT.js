68cbec3b3cdab1c3e3e6436ca5012e2bfe9bed29
Yuki Takahashi <yuki0627@gmail.com>
2022-10-20 17:58:23 +0900

    test: MemberNFTの前処理実装

--- test/MemberNFT.js
+++ test/MemberNFT.js
@@ -2,22 +2,25 @@ const {expect} = require("chai");
 const { ethers } = require("hardhat");
 
 describe("MemberNFTコントラクト", function () {
-    it("トークンの名前とシンボルがセットされている", async function () {
-        const name = "MemberNFT";
-        const symbol = "MEM";
-        const MemberNFT = await ethers.getContractFactory("MemberNFT");
-        const memberNFT = await MemberNFT.deploy();
+    let MemberNFT;
+    let memberNFT;
+    const name = "MemberNFT";
+    const symbol = "MEM";
+    let owner;
+    
+    beforeEach(async function () {
+        [owner] = await ethers.getSigners();
+        MemberNFT = await ethers.getContractFactory("MemberNFT");
+        memberNFT = await MemberNFT.deploy();
         await memberNFT.deployed();
+    });
 
+    it("トークンの名前とシンボルがセットされている", async function () {
         expect(await memberNFT.name()).to.equal(name);
         expect(await memberNFT.symbol()).to.equal(symbol);
 
     });
     it("デプロイアドレスがownerに設定されている", async function () {
-        const [owner] = await ethers.getSigners();
-        const MemberNFT = await ethers.getContractFactory("MemberNFT");
-        const memberNFT = await MemberNFT.deploy();
-        await memberNFT.deployed();
         expect(await memberNFT.owner()).to.equal(owner.address);
     });
-})
+});
